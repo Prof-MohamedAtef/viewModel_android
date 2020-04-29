993b6244ff1b17f3e80e12cce0664d36b100e2b0
Prof-Mohamed Atef <proofesser@gmail.com>
2020-04-29 19:16:05 +0200

    using SavedStateHandle in ViewModel

--- app/src/main/java/com/example/android/lifecycles/step6/SavedStateViewModel.java
+++ app/src/main/java/com/example/android/lifecycles/step6/SavedStateViewModel.java
@@ -18,20 +18,38 @@ package com.example.android.lifecycles.step6;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 public class SavedStateViewModel extends ViewModel {
 
     // TODO: Create constructor and use the LiveData from SavedStateHandle.
 
-    private MutableLiveData<String> name = new MutableLiveData<>();
+//    private MutableLiveData<String> name = new MutableLiveData<>();
 
     // Expose an immutable LiveData
-    LiveData<String> getName() {
-        return name;
+//    LiveData<String> getName() {
+//        return name;
+//    }
+
+//    void saveNewName(String newName) {
+//        name.setValue(newName);
+//    }
+
+
+    private static final String NAME_KEY = "name";
+
+    private SavedStateHandle mState;
+
+    public SavedStateViewModel(SavedStateHandle savedStateHandle) {
+        mState=savedStateHandle;
     }
 
-    void saveNewName(String newName) {
-        name.setValue(newName);
+    LiveData<String> getName(){
+        return mState.getLiveData(NAME_KEY);
+    }
+
+    void saveNewName(String newName){
+        mState.set(NAME_KEY, newName);
     }
 }
